d3b1a13948f04a5752b0d476e0955dd7b428af63
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-06-03 10:26:36 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -5,7 +5,7 @@
 #include <string>
 
 class Tree {
-private: 
+ private:
     struct Node {
         char value;
         std::vector<Node*> list;
@@ -48,7 +48,7 @@ private:
         }
     }
 
-public: 
+ public:
     std::string operator[](int i) const {
         if (i >= perm.size()) {
             return "";
